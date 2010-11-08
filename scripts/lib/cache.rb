9e5e9796e21d253691a5cb3e58b17d17ca971672
mangled <matthew@mangled.me>
2010-11-08 10:01:44 +0000

    under windows, for some reason, sqlite converts strings to integers if they are valid. bizzare, now explicitly convert

--- scripts/lib/cache.rb
+++ scripts/lib/cache.rb
@@ -66,7 +66,7 @@ module Poodle
 
         def find(uri)
             cached_url = @db.get_first_row("select * from urls where url = :url", :url => uri.to_s)
-            [cached_url[0].to_i, URI.parse(cached_url[1]), URI.parse(cached_url[2]), cached_url[3], cached_url[4]] if cached_url
+            [cached_url[0].to_i, URI.parse(cached_url[1]), URI.parse(cached_url[2]), cached_url[3].to_s, cached_url[4].to_s] if cached_url
         end
 
         def create_tables()
@@ -116,7 +116,7 @@ module Poodle
             else
                 @db.get_first_row("select * from urls where id > :id order by id", :id => @current_id)
             end
-            [cached_url[0].to_i, URI.parse(cached_url[1]), URI.parse(cached_url[2]), cached_url[3], cached_url[4]] if cached_url
+            [cached_url[0].to_i, URI.parse(cached_url[1]), URI.parse(cached_url[2]), cached_url[3].to_s, cached_url[4].to_s] if cached_url
         end
 
         def next_empty?(last_id)
